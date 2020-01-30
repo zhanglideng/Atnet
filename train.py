7b1c51ad7777ad32b96c0668bfb9e7313d7ed080
random <80976700@qq.com>
2020-01-30 22:37:56 +0800

    修改了部分错误。

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ accumulation_steps = 1  # 梯度积累的次数，类似于batch-size=64
 itr_to_lr = 10000 // BATCH_SIZE  # 训练10000次后损失下降50%
 itr_to_excel = 64 // BATCH_SIZE  # 训练64次后保存相关数据到excel
 loss_num = 5  # 包括参加训练和不参加训练的loss
-weight = [1, 1, 1, 1]
+weight = [1, 1, 1, 1, 1]
 train_haze_path = '/home/aistudio/data/data20070/nyu/train/'  # 去雾训练集的路径
 val_haze_path = '/home/aistudio/data/data20070/nyu/val/'  # 去雾验证集的路径
 gt_path = '/home/aistudio/data/data20070/nyu/gth/'
